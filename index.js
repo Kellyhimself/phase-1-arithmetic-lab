e709fba6040597b1e2c2c4e9cc3efed9a3a9cec3
Kitui Kelly <kituikelly@gmail.com>
2024-03-20 23:56:46 +0300

    completes the lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1=1;
+const num2=62;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+
+const num3 = 24;
+const num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(2,3,4,5,20)
